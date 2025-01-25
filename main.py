114a0a85b45e4f5c59ea2001b274be3d2ec014f5
Uppalapati Srikanth <srikanthuppalapati09@gmail.com>
2025-01-25 13:17:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-# from turtle import Turtle,Screen
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("brown4")
-# timmy.forward(100)
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+coffee = True
+while coffee:
+    user_choice = input("which one you want latte/espresso/cappuccino and do want see resource please click 'r' \n").lower()
+    if user_choice == "r":
+        coffee_maker.report()
+        money_machine.report()
+    elif user_choice == "off":
+        coffee = False
+        print("see you latter")
+    else:
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            print(money_machine.make_payment(drink.cost))
+            coffee_maker.make_coffee(drink)
 
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("pokeman names",["pikachu","squirtle","charmander"])
-table.add_column("type",["electric","water","fire"])
-print(table.align)
-print(table)
+
+
